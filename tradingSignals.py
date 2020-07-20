2c949188c8ce82c57544dbc36ad87988421fd70a
qTzzK <52429224+qTzzK@users.noreply.github.com>
2020-07-19 23:47:16 -0400

    Update tradingSignals.py

--- tradingSignals.py
+++ tradingSignals.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import xlrd
 
 filePath = r"C:\Users\keano\Desktop\BTC historical data.xlsx"
 xl = pd.ExcelFile(filePath)
@@ -7,10 +6,10 @@ df = xl.parse('3D', skiprows=2)
 buySignalPrice = 0
 sellSignalPrice = 0;
 pricesAtSignal = []
-countFivePercentIncrease = 0
-totalSignals = 0
+percentChange = 5
+countPercentChange = 0
 
-def get_change(current, previous):
+def GetPercentageChange(current, previous):
     if current == previous:
         return 0
     try:
@@ -18,30 +17,52 @@ def get_change(current, previous):
     except ZeroDivisionError:
         return float('inf')
 
+#populate array with buy/sell signals and their close prices
 for index, row in df.iterrows():
     if(row['Sell Alert'] == 1):
         pricesAtSignal.append(("Sell Alert", row['close']))
     elif(row['Buy Alert'] == 1):
         pricesAtSignal.append(("Buy Alert", row['close']))
-        
-for (alert,price) in pricesAtSignal:
-    totalSignals += 1
-    if(alert == "Buy Alert"):
-        if(buySignalPrice == 0):
-            buySignalPrice = price
-            print("Buy Signal Price: " + str(price))
-    elif(sellSignalPrice == 0):
-        sellSignalPrice = price
-        print("Sell Signal Price: " + str(price))
 
+def CalculatePercentageChange(sellToBuy):
+    global sellSignalPrice
+    global buySignalPrice
+    global countPercentChange
     if(sellSignalPrice != 0 and buySignalPrice != 0):
-        less = buySignalPrice > sellSignalPrice
-        percentChange = round(get_change(buySignalPrice, sellSignalPrice),1)
-        print("Percentage Change: " + ("-" if less else "+")  +  str(percentChange) + "%\n")
+        result = round(GetPercentageChange(buySignalPrice, sellSignalPrice),1)
+        less = sellSignalPrice > buySignalPrice if sellToBuy else buySignalPrice > sellSignalPrice
+        print("Percentage Change: " + ("-" if less else "+")  +  str(result) + "%\n")     
+        if(sellToBuy and less and result > percentChange or not less and result > percentChange):
+            countPercentChange += 1
         buySignalPrice = 0
         sellSignalPrice = 0
-        if(not less and percentChange > 5):
-            countFivePercentIncrease += 1
+        return result;
 
-print("\nBuy Signals followed by 5 percent increase: " + str(countFivePercentIncrease) + "/" + str(totalSignals) + "\n")
+#buy to sell
+for (alert,price) in pricesAtSignal:
+        if(alert == "Buy Alert"):
+            if(buySignalPrice == 0):
+                buySignalPrice = price
+                print("Buy Signal Price: " + str(price))
+        elif(sellSignalPrice == 0):
+            sellSignalPrice = price
+            print("Sell Signal Price: " + str(price))
+        CalculatePercentageChange(False)
+print("\nBuy Signals followed by "+str(percentChange)+" percent increase: " + str(countPercentChange) + "/" + str(len(pricesAtSignal)) + "\n")
+
+countPercentChange = 0           
+buySignalPrice = 0
+sellSignalPrice = 0
+
+#sell to buy
+for (alert,price) in pricesAtSignal:
+        if(alert == "Buy Alert"):
+            if(sellSignalPrice != 0):
+                buySignalPrice = price
+                print("Buy Signal Price: " + str(price))
+        elif(buySignalPrice == 0):
+            sellSignalPrice = price
+            print("Sell Signal Price: " + str(price))
+        CalculatePercentageChange(True)
+print("\nSell Signals followed by "+str(percentChange)+" percent decrease: " + str(countPercentChange) + "/" + str(len(pricesAtSignal)) + "\n")
 
