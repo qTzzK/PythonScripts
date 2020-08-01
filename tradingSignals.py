37e9f90baf56c39fdc63552a87e9bcd2dacda4d2
qTzzK <52429224+qTzzK@users.noreply.github.com>
2020-08-01 15:23:31 -0400

    Update tradingSignals.py
    refactor

--- tradingSignals.py
+++ tradingSignals.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-filePath = r"C:\Users\keano\Desktop\BTC historical data.xlsx"
+filePath = r"C:\Users\kamir\Desktop\BTC historical data.xlsx"
 xl = pd.ExcelFile(filePath)
 df = xl.parse('3D', skiprows=2)
 buySignalPrice = 0
@@ -25,48 +25,44 @@ for index, row in df.iterrows():
     elif(row['Buy Alert'] == 1):
         pricesAtSignal.append(("Buy Alert", row['close']))
 
-def CalculatePercentageChange(sellToBuy):
+def GetPriceSignalSuccessRate(buyToSell):
     global sellSignalPrice
     global buySignalPrice
     global countPercentChange
     global countComparisons
-    if(sellSignalPrice != 0 and buySignalPrice != 0):
-        countComparisons += 1
-        result = round(GetPercentageChange(buySignalPrice, sellSignalPrice),1) if sellToBuy else round(GetPercentageChange(sellSignalPrice, buySignalPrice),1)
-        less = sellSignalPrice > buySignalPrice if sellToBuy else buySignalPrice > sellSignalPrice
-        print("Percentage Change: " + ("-" if less else "+")  +  str(result) + "%\n")     
-        if(sellToBuy and less and result > percentChange or not sellToBuy and not less and result > percentChange):
-            countPercentChange += 1
-        buySignalPrice = 0
-        sellSignalPrice = 0
-        return result;
 
-#buy to sell
-for (alert,price) in pricesAtSignal:
+    for (alert,price) in pricesAtSignal:
         if(alert == "Buy Alert"):
-            if(buySignalPrice == 0):
+            if(buyToSell):
+                if(buySignalPrice == 0):
+                    buySignalPrice = price
+                    #print("Buy Signal Price: " + str(price))
+            elif(sellSignalPrice != 0):
                 buySignalPrice = price
-                print("Buy Signal Price: " + str(price))
-        elif(sellSignalPrice == 0):
-            sellSignalPrice = price
-            print("Sell Signal Price: " + str(price))
-        CalculatePercentageChange(False)
-print("\nBuy Signals followed by "+str(percentChange)+" percent increase: " + str(countPercentChange) + "/" + str(countComparisons) + "\n")
-
-countPercentChange = 0           
-countComparisons = 0
-buySignalPrice = 0
-sellSignalPrice = 0
-
-#sell to buy
-for (alert,price) in pricesAtSignal:
-        if(alert == "Buy Alert"):
-            if(sellSignalPrice != 0):
-                buySignalPrice = price
-                print("Buy Signal Price: " + str(price))
+        elif(buyToSell):
+            if(sellSignalPrice == 0):
+                sellSignalPrice = price
+                #print("Sell Signal Price: " + str(price))
         elif(buySignalPrice == 0):
-            sellSignalPrice = price
-            print("Sell Signal Price: " + str(price))
-        CalculatePercentageChange(True)
-print("\nSell Signals followed by "+str(percentChange)+" percent decrease: " + str(countPercentChange) + "/" + str(countComparisons) + "\n")
+                sellSignalPrice = price
 
+        if(sellSignalPrice != 0 and buySignalPrice != 0):
+            countComparisons += 1
+            result = round(GetPercentageChange(buySignalPrice, sellSignalPrice),1) if not buyToSell else round(GetPercentageChange(sellSignalPrice, buySignalPrice),1)
+            less = sellSignalPrice > buySignalPrice if not buyToSell else buySignalPrice > sellSignalPrice
+            #print("Percentage Change: " + ("-" if less else "+")  +  str(result) + "%\n")     
+            if(not buyToSell and less and result > percentChange or buyToSell and not less and result > percentChange):
+                countPercentChange += 1
+            buySignalPrice = 0
+            sellSignalPrice = 0
+
+    print(("Buy" if buyToSell else "Sell") + " Signals followed by "+str(percentChange)+" percent " + ("increase:" if buyToSell else "decrease:") + str(countPercentChange) + "/" + str(countComparisons) + "\n")
+
+    #reset 
+    countPercentChange = 0           
+    countComparisons = 0
+    buySignalPrice = 0
+    sellSignalPrice = 0
+
+GetPriceSignalSuccessRate(False)
+GetPriceSignalSuccessRate(True)
